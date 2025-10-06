8d7ea67b3a56bf6031e09ea1566596818f46f622
uyenntp207 <phuonguyennguyen160707@gmail.com>
2025-10-06 10:47:29 +0700

    dangky

---
+++ src/test/java/com/example/xuongthuchanh/XuongThucHanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xuongthuchanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuongThucHanhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
